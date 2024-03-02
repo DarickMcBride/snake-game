cd8ff2cd6fd1b98664686aadc20d0ef2ab505048
Darick McBride <darickmcbride@me.com>
2024-03-01 22:14:15 -0800

    completed game

---
+++ .venv/main.py
@@ -0,0 +1,48 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from score import Score
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkey(snake.move_up, "Up")
+screen.onkey(snake.move_down, "Down")
+screen.onkey(snake.move_left, "Left")
+screen.onkey(snake.move_right, "Right")
+
+is_On = True
+while is_On:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        score.increase_score()
+        food.refresh()
+        snake.grow_snake()
+
+    # Detect collision with wall
+    if (snake.head.xcor() > 280 or snake.head.xcor() < -280
+            or snake.head.ycor() > 280 or snake.head.ycor() < -280):
+        is_On = False
+        score.game_over()
+
+    # Detect collision with tail
+
+    for segment in segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_On = False
+            score.game_over()
+
+screen.exitonclick()
